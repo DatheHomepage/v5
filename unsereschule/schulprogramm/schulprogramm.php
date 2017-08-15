4a4cf3363d58307ff10287951c072cff4c89598c
Oisin Smith <oisin@oisinsmith.eu>
2017-08-15 20:33:54 +0100

    Abstände im Menü verbessert

--- unsereschule/schulprogramm/schulprogramm.php
+++ unsereschule/schulprogramm/schulprogramm.php
@@ -1,46 +1,40 @@
+<div class="titel">- Schulprogramm -</div>
+<div class="table-responsive">
+    <table>
+        <tr>
+            <td>
+                Am Dienstag, den 11. Dezember 2012 wurde die überarbeitete zweite Fassung unseres Schulprogrammes
+                von der Schulkonferenz einstimmig angenommen und damit der Schulaufsicht zur Genehmigung vorgelegt.
+            </td>
+            <td>
+                Ein Auszug zeigt unser Leitbild:
+                <br>
+                <img class="bild-xl" alt="" src="/unsereschule/schulprogramm/leitbild_dathe.jpg">
+            </td>
+        </tr>
+        <tr>
+            <td colspan="2">
+                <b>Schulprogramm der Dathe-OS</b>
+                <br>Die 2. Fassung des Schulprogramms wurde nun durch die zuständige Schulaufsicht genehmigt. Damit
+                findet ein 2,5-jähriger Prozess mit intensiver Arbeit und Diskussion aller an Schule Beteiligten
+                seinen vorläufigen Abschluss.
+                <br> Im Genehmigungsschreiben heißt es: " ... Das Programm überzeugt inhaltlich und formal
+                uneingeschränkt. Besonders würdigen möchte ich die prägnanten Erläuterungen der
+                Entwicklungsschwerpunkte. ... den aktiv Beteiligten ist eine persönliche, adressatenbezogene
+                Darstellung gelungen, die den Stand und das Wohin der Schulgemeinschaft hervorragend verdeutlicht.
+                ... Ich bedanke mich bei allen Beteiligten für die geleistete Arbeit und wünsche Kreativität, Erfolg
+                und Kraft bei der täglichen pädagogischen Arbeit."
+                <br> Diesem Dank schließe ich mich als Schulleiterin an und freue mich über die Veröffentlichung auf
+                unserer Homepage.
+                <br> Schulze
+                <br> Schulleiterin
+                <br>
 
-<body style="background-color: transparent;" leftmargin="0" topmargin="0" alink="#000000" link="#000000" vlink="#000000">
-
-    <div class="titel">- Schulprogramm -</div>
-    <br>
-    <div class="teaser">
-        <br>
-
-        <br>
-
-        <table border=0 width=90% align=center cellspacing=0>
-            <tr>
-                <td>
-                    <div class="teaser">
-                        Am Dienstag, den 11. Dezember 2012 wurde die überarbeitete zweite Fassung unseres Schulprogrammes von der Schulkonferenz einstimmig angenommen
-                        <br> und damit der Schulaufsicht zur Genehmigung vorgelegt.
-                    </div>
-                </td>
-                <td>
-                    <div class="teaser">
-                        Ein Auszug zeigt unser Leitbild:
-                        <br>
-                        <img style="width: 300px; height: 275px; float: right;" alt="" src="./unsereschule/schulprogramm/leitbild_dathe.jpg" hspace="5" vspace="10">
-                    </div>
-                </td>
-            </tr>
-            <tr>
-                <td colspan=2>
-                    <div class="teaser">
-                        <b>Schulprogramm der Dathe-OS</b>
-                        </a>
-                        <br>Die 2. Fassung des Schulprogramms wurde nun durch die zuständige Schulaufsicht genehmigt. Damit findet ein 2,5-jähriger Prozess mit intensiver Arbeit und Diskussion aller an Schule Beteiligten seinen vorläufigen Abschluss.
-                        <br> Im Genehmigungsschreiben heißt es: " ... Das Programm überzeugt inhaltlich und formal uneingeschränkt. Besonders würdigen möchte ich die prägnanten Erläuterungen der Entwicklungsschwerpunkte. ... den aktiv Beteiligten ist eine persönliche, adressatenbezogene Darstellung gelungen, die den Stand und das Wohin der Schulgemeinschaft hervorragend verdeutlicht. ... Ich bedanke mich bei allen Beteiligten für die geleistete Arbeit und wünsche Kreativität, Erfolg und Kraft bei der täglichen pädagogischen Arbeit."
-                        <br> Diesem Dank schließe ich mich als Schulleiterin an und freue mich über die Veröffentlichung auf unserer Homepage.
-                        <br> Schulze
-                        <br> Schulleiterin
-                    </div>
-                    <br>
-                    <div class="teaser">
-
-                        <a href="./unsereschule/schulprogramm/Schulprogramm_2012_Teil1.pdf" target="_Blank"><b>Schulprogramm zur Ansicht Teil 1</b></a>
-                        <br>
-                        <a href="./unsereschule/schulprogramm/Schulprogramm_2012_Teil2.pdf" target="_Blank"><b>Schulprogramm zur Ansicht Teil 2</b></a><!--
+                <a href="/unsereschule/schulprogramm/Schulprogramm_2012_Teil1.pdf" target="_Blank"><b>Schulprogramm
+                        zur Ansicht Teil 1</b></a>
+                <br>
+                <a href="/unsereschule/schulprogramm/Schulprogramm_2012_Teil2.pdf" target="_Blank"><b>Schulprogramm
+                        zur Ansicht Teil 2</b></a><!--
                         <br>
                         <a href="./unsereschule/schulprogramm/Schulprogramm_2012_TeilA.pdf" target="_Blank"><b>Schulprogramm zur Ansicht Teil A</b></a>
                         <br>
@@ -50,24 +44,16 @@
                         <br>
                         <a href="./unsereschule/schulprogramm/Schulprogramm_2012_TeilD.pdf" target="_Blank"><b>Schulprogramm zur Ansicht Teil D</b></a>
                         <br>-->
-						<br>
-                        <a href="./unsereschule/schulprogramm/Anhang_zum_Schulprogramm_Dathe_2012.pdf" target="_Blank"><b>Anhang zum Schulprogramm </b></a>
-                        <br>
-                        <a href="./unsereschule/schulprogramm/Schulprogramm_2012_druck.pdf" target="_Blank"><b>Schulprogramm zum Ausdrucken </b></a>
-                        <br>
-                    </div>
-                </td>
-            </tr>
-        </table>
-
-
-        <br>
-        <hr>
-        <br>
-        <br>
-
-
-    </div>
-
-
-</body>
+                <br>
+                <a href="/unsereschule/schulprogramm/Anhang_zum_Schulprogramm_Dathe_2012.pdf" target="_Blank"><b>Anhang
+                        zum Schulprogramm </b></a>
+                <br>
+                <a href="/unsereschule/schulprogramm/Schulprogramm_2012_druck.pdf" target="_Blank"><b>Schulprogramm
+                        zum Ausdrucken </b></a>
+                <br>
+            </td>
+        </tr>
+    </table>
+</div>
+<br>
+<hr>
