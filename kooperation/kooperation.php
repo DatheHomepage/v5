5b8c5b8b0be0230aeeec553f79802b71b5c98ccd
Oisin Smith <oisin@oisinsmith.eu>
2017-08-16 01:18:15 +0100

    Kooperation Logo ersetzt

--- kooperation/kooperation.php
+++ kooperation/kooperation.php
@@ -1,18 +1,15 @@
 <div class="titel">- Kooperationen -</div>
+Auf den folgenden Seiten finden Sie eine &Uuml;bersicht &uuml;ber die Koorperationspartner unserer Schule
 <br>
-<div class="teaser">
-    Auf den folgenden Seiten finden Sie eine &Uuml;bersicht &uuml;ber die Koorperationspartner unserer Schule
-    <br>
-    <br>
-    <a href="?sect=./kooperation/allgemein/allgemein">Allgemeine Koorperationen<br></a>
-    <br>
-    <a href="?sect=./kooperation/sport/sport">Koorperationen des Fachbereiches Sport</a>
-    <br>
-    <br>
-    <a href="?sect=./kooperation/biologie/biologie">Koorperationen des Fachbereiches Biologie</a>
-    <br>
-    <br>
-    <br>
-    <img src="./kooperation/image035.jpg">
-    <img src="./kooperation/alba_klein.jpg">
-</div>
+<br>
+<a href="?sect=./kooperation/allgemein/allgemein">Allgemeine Koorperationen<br></a>
+<br>
+<a href="?sect=./kooperation/sport/sport">Koorperationen des Fachbereiches Sport</a>
+<br>
+<br>
+<a href="?sect=./kooperation/biologie/biologie">Koorperationen des Fachbereiches Biologie</a>
+<br>
+<br>
+<br>
+<img src="/kooperation/image035.jpg">
+<img src="/static/bilder/logos/alba.png" class="bild-s">
